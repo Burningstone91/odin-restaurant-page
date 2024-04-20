61c289747a441fe5d8fe518430d03745118c7057
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-04-21 00:12:15 +0200

    Add nav buttons to HTML and configure webpack to use index.html correctly

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
