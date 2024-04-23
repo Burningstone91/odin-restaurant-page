587f13e3b7c5f5d1f6d570859a01a708f64032cc
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-04-23 22:47:20 +0200

    Style contact page and add background image

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,23 @@
 function renderContactPage(contentDiv) {
+  const contact = document.createElement("div");
   const image = document.createElement("img");
   const title = document.createElement("h1");
   const phone = document.createElement("p");
   const address = document.createElement("p");
 
+  contact.classList.add("contact");
   image.src =
     "https://cdn.pixabay.com/photo/2018/01/31/05/43/web-3120321_1280.png";
   title.textContent = "Contact Us";
   phone.textContent = "📱 +423 888 88 88";
   address.textContent = "Tomatostreet 12, 8055 Mozzarellahausen, Pizzaland";
 
-  contentDiv.appendChild(image);
-  contentDiv.appendChild(title);
-  contentDiv.appendChild(phone);
-  contentDiv.appendChild(address);
+  contact.appendChild(image);
+  contact.appendChild(title);
+  contact.appendChild(phone);
+  contact.appendChild(address);
+
+  contentDiv.appendChild(contact);
 }
 
 export { renderContactPage };
