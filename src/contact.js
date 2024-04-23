94ce43b3e7208e04c6376414a9e9ab42d6b0c08f
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-04-23 21:05:50 +0200

    Add content for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,19 @@
 function renderContactPage(contentDiv) {
-  const page = document.createElement("div");
-  page.classList.add("content");
+  const image = document.createElement("img");
+  const title = document.createElement("h1");
+  const phone = document.createElement("p");
+  const address = document.createElement("p");
 
-  const title = document.createElement("p");
-  title.textContent = "Title for Contact Page";
+  image.src =
+    "https://cdn.pixabay.com/photo/2018/01/31/05/43/web-3120321_1280.png";
+  title.textContent = "Contact Us";
+  phone.textContent = "📱 +423 888 88 88";
+  address.textContent = "Tomatostreet 12, 8055 Mozzarellahausen, Pizzaland";
 
-  page.appendChild(title);
-
-  contentDiv.appendChild(page);
+  contentDiv.appendChild(image);
+  contentDiv.appendChild(title);
+  contentDiv.appendChild(phone);
+  contentDiv.appendChild(address);
 }
 
 export { renderContactPage };
