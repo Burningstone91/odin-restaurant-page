486e53229c08d57d638b581c882b22d134ad0a60
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-04-23 21:36:34 +0200

    Add content for menu page

--- src/about.js
+++ src/about.js
@@ -4,7 +4,7 @@ function renderAboutPage(contentDiv) {
   const text = document.createElement("p");
 
   image.src =
-    "https://cdn.pixabay.com/photo/2024/02/21/06/48/ai-generated-8586855_1280.png";
+    "https://cdn.pixabay.com/photo/2020/06/28/14/09/dough-5349412_1280.jpg";
   title.textContent = "Our Story";
   text.innerHTML =
     "Nestled in the heart of a bustling city, Burni’s Diner is more than just a pizzeria—it’s a slice of culinary heaven crafted by Antonio Burni, a passionate pizza aficionado. Inspired by his grandmother’s kitchen in Naples, Antonio set out to create a place where every bite would transport patrons to Italy’s cobblestone streets. <br><br> With the aroma of freshly baked pizza dough and simmering marinara sauce drawing in hungry customers, Burni’s became a beloved fixture in the community. Families gathered around checkered tablecloths, friends toasted over glasses of Chianti, and couples savored romantic evenings by candlelight. <br><br> But Burni’s Diner isn’t just about delicious food; it’s about giving back. Through fundraisers for local charities and donations to those in need, Antonio and his team spread the spirit of generosity. <br><br>Come join us at Burni’s, where every slice tells a story, and every bite is a taste of amore.";
--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,50 @@
 function renderMenuPage(contentDiv) {
-  const page = document.createElement("div");
-  page.classList.add("content");
+  const title = document.createElement("h1");
+  title.textContent = "Menu";
 
-  const title = document.createElement("p");
-  title.textContent = "Title for Menu Page";
+  contentDiv.appendChild(title);
 
-  page.appendChild(title);
+  contentDiv.appendChild(
+    createMenu(
+      "Margherita",
+      "Pizzadough, tomato sauce, mozzarella, basile",
+      "https://cdn.pixabay.com/photo/2020/06/08/16/49/pizza-5275191_1280.jpg",
+    ),
+  );
 
-  contentDiv.appendChild(page);
+  contentDiv.appendChild(
+    createMenu(
+      "Pizza Salame",
+      "Pizzadough, tomato sauce, mozzarella, salame, basile",
+      "https://cdn.pixabay.com/photo/2016/04/21/22/50/pizza-1344720_1280.jpg",
+    ),
+  );
+
+  contentDiv.appendChild(
+    createMenu(
+      "Pizza ai Funghi",
+      "Pizzadough, tomato sauce, mozzarella, mushrooms, olives, basile",
+      "https://cdn.pixabay.com/photo/2014/07/08/12/34/pizza-386717_1280.jpg",
+    ),
+  );
+}
+
+function createMenu(name, desc, imgLink) {
+  const menu = document.createElement("div");
+  const img = document.createElement("img");
+  const title = document.createElement("h1");
+  const description = document.createElement("p");
+
+  menu.classList.add("menu");
+  img.src = imgLink;
+  title.textContent = name;
+  description.textContent = desc;
+
+  menu.appendChild(img);
+  menu.appendChild(title);
+  menu.appendChild(description);
+
+  return menu;
 }
 
 export { renderMenuPage };
